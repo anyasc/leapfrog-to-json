65da28ae712e7969c59bc18acf092b411dd345da
Anya Cunha <anyacontato@gmail.com>
2025-09-04 13:36:29 -0300

    debug 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+console.log("App está carregando...");
+console.log("Base URL:", import.meta.env.BASE_URL);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
